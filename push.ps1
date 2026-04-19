7a0d10b31e1c0fd0b43a843d3a440fd8d77da46c
Made by Anto <soyantoo@proton.me>
2026-04-19 11:17:09 +0200

    update pfp+username

--- push.ps1
+++ push.ps1
@@ -1,21 +1,27 @@
+$remote = git remote
+
+if (-not $remote) {
+    Write-Host "⚠ Nessun remote configurato!"
+    Write-Host "Inserisci URL della repo GitHub:"
+    $url = Read-Host
+
+    git remote add origin $url
+    git branch -M main
+}
+
 Write-Host "Vuoi fare commit + push? (Y/N)"
 $answer = Read-Host
 
 if ($answer -eq "Y" -or $answer -eq "y") {
 
-    Write-Host "Aggiungo file..."
     git add .
 
-    Write-Host "Inserisci messaggio commit:"
+    Write-Host "Messaggio commit:"
     $msg = Read-Host
 
     git commit -m "$msg"
 
-    Write-Host "Push in corso..."
-    git push
+    git push -u origin main
 
     Write-Host "Fatto ✔"
-}
-else {
-    Write-Host "Operazione annullata"
 }
